967bf1bd93687ed1419840748eae9eb1ef08b870
KM <the.real.laplace@gmail.com>
2022-11-22 19:38:14 +0200

    wtf

--- src/_old/atom.js
+++ src/_old/atom.js
@@ -9,18 +9,20 @@ const plugin = (R) => {
 		}
 
 		_createNode () {
-			this._node = new this._Node()
-			this._node._atom = this
-			this._node.onChange = this._onChange.bind(this)
+			const node = this._node = new this._Node()
+			node._atom = this
+			node.onChange = this._onChange.bind(this)
 
-			logger && logger.log("subscribed", this._node.toString())
+			logger && logger.log("subscribed", node.toString())
 		}
 
 		_destroyNode () {
-			logger && logger.log("unsubscribed", this._node.toString())
-
-			this._node.remove()
+			const node = this._node
 			this._node = null
+
+			logger && logger.log("unsubscribed", node.toString())
+
+			node.remove()
 		}
 
 		_onChange (prop, value) {
